101ff0a6be20127fd5248d744f5f71e0971df8f3
boukharSoufiane1998 <boukhar.soufiane.coding@gmail.com>
2024-02-26 16:38:41 +0100

    update travail

--- app/resources/views/layouts/menu.blade.php
+++ app/resources/views/layouts/menu.blade.php
@@ -1,6 +1,14 @@
 <li class="nav-item">
     <a href="{{ route('membre.index') }}" class="nav-link {{ Request::is('membre.index') ? 'active' : '' }}">
       <i class="nav-icon fas fa-users"></i>
-      <p>Personnes</p>
+      <p>Membres</p>
     </a>
 </li>
+
+<li class="nav-item">
+  <a href="{{ route('client.index') }}" class="nav-link {{ Request::is('client.index') ? 'active' : '' }}">
+    <i class="nav-icon fas fa-users"></i>
+    <p>Clients</p>
+  </a>
+</li>
+
